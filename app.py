b65b0c330580666d46d966795d1fe3ea4e1fe34f
ivorobyev <vorobyevilia@gmail.com>
2019-12-04 15:31:51 +0300

    bug fixes, exception processing, style changes

--- app.py
+++ app.py
@@ -10,12 +10,16 @@ def render_form():
 @app.route('/calc', methods = ['POST'])
 def calculate():
     seq = request.form['sequence'].upper()
-    mutate_first_codon = request.form['mutate']
+    mutate_first_codon = int(request.form['mutate'])
     codon_table = sn.get_codon_table(request.form['codon_table'])
-    max_nmut = request.form['max_mutations']
+    max_nmut = int(request.form['max_mutations'])
+
+    if len(seq)%3 != 3:
+        raise Exception('Number of nucleotides doesn\'t divide into 3')
+
     s = sn.get_sequence_number(seq, mutate_first_codon, max_nmut, codon_table)
     return s
 
 if __name__ == '__main__':
     app.run()
-
+    
