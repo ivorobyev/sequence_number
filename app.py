840a2e13752c565935e4c08137aaaba63e8b9381
ivorobyev <vorobyevilia@gmail.com>
2019-12-05 16:48:24 +0300

    add multiple input and output

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, json
 import sequence_number as sn
-import Bio
+import os
+from Bio import SeqIO
 
 app = Flask(__name__)
 
@@ -10,18 +11,21 @@ def render_form():
 
 @app.route('/calc', methods = ['POST'])
 def calculate():
-    seq = request.form['sequence'].upper()
     file_ =  request.files['seq']
     mutate_first_codon = int(request.form['mutate'])
     codon_table = sn.get_codon_table(request.form['codon_table'])
     max_nmut = int(request.form['max_mutations'])
 
-    #fasta_sequences = SeqIO.parse(open(file_),'fasta')
+    records = {}
+    for record in SeqIO.parse(file_.filename, "fasta"):
+        records[record.id] = str(record.seq)
 
-    print(file_)
+    result = {}
+    for a in records:
+        result[a] = sn.get_sequence_number(records[a], mutate_first_codon, max_nmut, codon_table)
 
-    s = sn.get_sequence_number(seq, mutate_first_codon, max_nmut, codon_table)
-    return s
+    j = json.dumps(result)
+    return j
 
 if __name__ == '__main__':
     app.run()
--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,26 @@
-function calculate() {
-    if ($.trim($('#name').val()) == ""){
-        alert('Empty sequence')
-        exit()
-    }
+function make_table(res_list){
+    res_list = res_list.trim()
+    seq_numbers = res_list.split(' ')
+            
+    var perrow = 3,
+    tbl = "<table id = 'sequence_number'><tr><th>Number of nucleotide mutaions</th><th>Possible amino acid sequences</th><th>Integral number of amino acid sequences</th></tr><tr>";
 
+    for (var i=0; i<seq_numbers.length; i++) {
+        tbl += "<td>" + seq_numbers[i] + "</td>";
+        var next = i+1;
+        if (next%perrow==0 && next!=seq_numbers.length) {
+            tbl += "</tr><tr>";
+        }
+    }
+    tbl += "</tr></table>";
+    tbl += '<br/><a download="sequnce_number.csv" href="#" onclick="return ExcellentExport.csv(this, \'sequence_number\');">Export to CSV</a>'
+
+    $('#result').show()
+    $('#len').html(tbl) 
+
+}
+
+function calculate() {
     var file = $('#seq_file');
 
     if (file.prop('files').length) {
@@ -21,24 +38,7 @@ function calculate() {
         processData: false, 
         contentType: false,
         success: function(response) {
-            response = response.trim()
-            seq_numbers = response.split(' ')
-            
-            var perrow = 3,
-            tbl = "<table id = 'sequence_number'><tr><th>Number of nucleotide mutaions</th><th>Possible amino acid sequences</th><th>Integral number of amino acid sequences</th></tr><tr>";
-
-            for (var i=0; i<seq_numbers.length; i++) {
-                tbl += "<td>" + seq_numbers[i] + "</td>";
-                var next = i+1;
-                if (next%perrow==0 && next!=seq_numbers.length) {
-                    tbl += "</tr><tr>";
-                }
-            }
-            tbl += "</tr></table>";
-            tbl += '<br/><a download="sequnce_number.csv" href="#" onclick="return ExcellentExport.csv(this, \'sequence_number\');">Export to CSV</a>'
-
-            $('#result').show()
-            $('#len').html(tbl) 
+            make_table(response)
         },
         error: function(response) {
             $('#result').show()
