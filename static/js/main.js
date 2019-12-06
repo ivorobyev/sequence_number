0ad522b67d1b1f47ea37671581182e1c92d664d0
ivorobyev <vorobyevilia@gmail.com>
2019-12-06 12:47:51 +0300

    style change

--- static/js/main.js
+++ static/js/main.js
@@ -9,7 +9,7 @@ function make_table(results, t_name){
     if (err_msg.indexOf('ERROR') == -1){
         tbl = "<div class = 'res_table'><h3>"+t_name+"</h3><table id = '"+t_name+"'><tr><th>Number of nucleotide mutaions</th><th>Possible amino acid sequences</th><th>Integral number of amino acid sequences</th></tr><tr>";
         if (err_msg.indexOf('WARNING') != -1){
-            tbl += "<p class = 'error'>"+err_msg+"</p>"
+            tbl += "<p class = 'warning'>"+err_msg+"</p>"
         }
         for (var i=0; i<seq_numbers.length; i++) {
             tbl += "<td>" + seq_numbers[i] + "</td>";
@@ -21,8 +21,6 @@ function make_table(results, t_name){
         tbl += "</tr></table>";
         tbl += "<br/><a download=\"sequnce_number.csv\" href=\"#\" onclick=\"return ExcellentExport.csv(this, \'"+t_name+"\');\">Export to CSV</a>"
         tbl += "</div>"
-
-        
     }
     else {
         tbl = "<div class = 'res_table'><h3>"+t_name+"</h3><p class = 'error'>"+err_msg+"</p></div>"
@@ -34,8 +32,12 @@ function calculate() {
     var file = $('#seq_file');
 
     if (file.prop('files').length) {
-      var formData = new FormData($('form')[0]);
-      formData.append('seq', file.prop('files')[0]);
+        var formData = new FormData($('form')[0]);
+        formData.append('seq', file.prop('files')[0]);
+    }
+    else {
+        alert('Empty file')
+        exit()
     }
 
     event.preventDefault();
